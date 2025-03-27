09de0677115942b41f7bc608ecc90067c58b28c6
Kar <c030324037@mahasiswa.poliban.ac.id>
2025-03-27 16:17:16 +0800

    Praktek Array menangani data string

--- tugas2/arrayKelas.cpp
+++ tugas2/arrayKelas.cpp
@@ -3,4 +3,38 @@
 using std::cout;
 using std::string;
 
+
 string kelas[30];
+int pos = 0;
+
+void input_data(string nama);
+void output_data();
+
+
+int main()
+{
+    input_data("");
+    output_data();
+    input_data("Mie Ayam sedap");
+    output_data();
+    input_data("Minum air putih");
+    output_data();
+
+    return 0;
+}
+
+
+void input_data(string nama)
+{
+    kelas [pos] = nama;
+    pos++;
+}
+
+void output_data()
+{
+    for(int i = 0; i < pos; i++){
+        cout << kelas[i];
+        (i < pos - 1) ? cout << ", " : cout << "." << std::endl;
+    }
+
+}
